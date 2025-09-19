3177d1998c1dfb1dc0eeee0ca96e3593a21ed45e
PawelRa <pawelrat@poczta.onet.pl>
2025-09-19 19:11:36 +0200

    ładowanie danych

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import boto3
+import pandas as pd
 import pathlib
+from dotenv import load_dotenv
 
 BUCKET_NAME = "dane-modul9"
+
+load_dotenv()
+
+s3 = boto3.client(
+    "s3",
+)
+
+wroclaw_2023_df = pd.read_csv(f"s3://{BUCKET_NAME}/dane-zadanie_modul9/halfmarathon_wroclaw_2023__final.csv", sep=";")
+wroclaw_2024_df = pd.read_csv(f"s3://{BUCKET_NAME}/dane-zadanie_modul9/halfmarathon_wroclaw_2024__final.csv", sep=";")
+
