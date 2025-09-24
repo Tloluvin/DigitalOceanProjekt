cb1aac31a10b2dd9ee5ec45edd8d82265b419a3d
PawelRa <pawelrat@poczta.onet.pl>
2025-09-24 10:22:09 +0200

    Struktura strony z przeglądem

--- app.py
+++ app.py
@@ -12,43 +12,32 @@ s3 = boto3.client(
     "s3",
 )
 
+st.set_page_config(page_title="Zadanie z modułu 9", page_icon="📊", layout="wide")
+
 wroclaw_2023_df = pd.read_csv(f"s3://{BUCKET_NAME}/dane-zadanie_modul9/halfmarathon_wroclaw_2023__final.csv", sep=";")
 wroclaw_2024_df = pd.read_csv(f"s3://{BUCKET_NAME}/dane-zadanie_modul9/halfmarathon_wroclaw_2024__final.csv", sep=";")
 
-print ("Podstawowe informacje o ramce danych")
-wroclaw_2023_df.info()
-wroclaw_2024_df.info()
+st.set_page_config(layout="wide")
 
-print ("Zliczenie braków w każdej kolumnie")
-wroclaw_2023_df.isna().sum()
-wroclaw_2024_df.isna().sum()
+# Sidebar menu
+st.sidebar.title("Menu")
+menu_selection = st.sidebar.radio(
+    "Wybierz sekcję:",
+    ("Przegląd danych", "Analiza EDA", "Model predykcji")
+)
 
-print ("Typy i liczba braków w jednej tabeli")
-pd.DataFrame({
-    "dtype": wroclaw_2023_df.dtypes,
-    "missing": wroclaw_2023_df.isna().sum()
-})
+# Dynamiczny tytuł w zależności od sekcji
+if menu_selection == "Przegląd danych":
+    st.title("Przegląd danych półmaratonu")
+    st.subheader("Dane za rok 2023")
+    st.dataframe(wroclaw_2023_df)
+    st.subheader("Dane za rok 2024")
+    st.dataframe(wroclaw_2024_df)
 
-pd.DataFrame({
-    "dtype": wroclaw_2024_df.dtypes,
-    "missing": wroclaw_2024_df.isna().sum()
-})
+elif menu_selection == "Analiza EDA":
+    st.title("Eksploracyjna analiza danych (EDA)")
+    st.write("Tu pojawi się szczegółowa analiza danych.")
 
-print("Podgląd wartości w kolumnach czasowych")
-
-wroclaw_2023_df[["5 km Czas", "10 km Czas", "15 km Czas", "20 km Czas", "Czas"]].head(10)
-
-wroclaw_2024_df[["5 km Czas", "10 km Czas", "15 km Czas", "20 km Czas", "Czas"]].head(10)
-
-print("Sprawdzenie unikalnych wartości (formatów)")
-wroclaw_2023_df["Czas"].dropna().unique()[:20]
-wroclaw_2024_df["Czas"].dropna().unique()[:20]
-
-print("Test konwersji na timedelta")
-pd.to_timedelta(wroclaw_2023_df["Czas"].dropna().head(10))
-pd.to_timedelta(wroclaw_2023_df["Czas"].dropna().head(10))
-
-
-
-st.set_page_config(page_title="Zadanie z modułu 9", page_icon="📊", layout="wide")
-st.title("Zadanie z modułu 9")
+elif menu_selection == "Model predykcji":
+    st.title("Model przewidujący wyniki biegu")
+    st.write("Tu pojawi się narzędzie przewidujące rezultaty na podstawie podanych danych zawodnika.")
